b59faafdd4bbdea8dcc341b73f56c85eb9433385
Lakshya Dhawan <lakshyad1902@gmail.com>
2026-06-20 12:14:11 +0530

    token fix

--- apisP.py
+++ apisP.py
@@ -70,7 +70,7 @@ with open("todos_stats.json", "w") as fileS:
 import requests
 
 base_url3 = "https://api.restcountries.com/countries/v5"
-headers = {"Authorization": "Bearer rc_live_35e0adc91c594cbe906c38cd5d943e48"}
+# headers = {"Authorization": "Bearer rc_live_35e0adc91c594cbe906c38cd5d943e48"}
 
 response3 = requests.get(base_url3, headers=headers)
 
